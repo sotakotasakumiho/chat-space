25b16cf8abd99567b622175d915a59624e389e34
sotakotasakumiho <shiomisoutarou@gmail.com>
2020-06-09 19:27:09 +0900

    ok

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id= ${message.id}>
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id= ${message.id}>
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -39,27 +39,53 @@ $(function(){
      return html;
    };
  }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.group-info__massages').append(html);
-    $('form')[0].reset();
-    $('.group-info').animate({ scrollTop: $('.group-info')[0].scrollHeight});
-    $('.submit-btn').prop('disabled', false);
+ $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-  });
-})
+   .done(function(data){
+     var html = buildHTML(data);
+     $('.group-info__massages').append(html);
+     $('form')[0].reset();
+     $('.group-info').animate({ scrollTop: $('.group-info')[0].scrollHeight});
+     $('.submit-btn').prop('disabled', false);
+   })
+   .fail(function() {
+     alert("メッセージ送信に失敗しました");
+   });
+   
+ });
+ var reloadMessages = function() {
+   var last_message_id = $('.message:last').data("message-id");
+   $.ajax({
+     url: "api/messages",
+     type: "get",
+     dataType: 'json',
+     data: {id: last_message_id}
+   })
+   .done(function(messages) {
+     if (messages.length !== 0) {
+     var insertHTML = '';
+     $.each(messages,function(i,message){
+       insertHTML += buildHTML(message)
+     });
+     $('.group-info__massages').append(insertHTML);
+     $('.group-info').animate({ scrollTop: $('.group-info')[0].scrollHeight});
+     }
+   })
+   .fail(function() {
+     alert('error');
+   });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
